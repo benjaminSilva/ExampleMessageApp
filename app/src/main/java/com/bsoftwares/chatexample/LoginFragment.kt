b96736dbd354c4fd45378d5608d21e9b42dff33c
Benjamin Silva <bennycsp@gmail.com>
2020-11-12 23:29:00 -0500

    Implementing Firebase, still going to apply ViewModel

--- app/src/main/java/com/bsoftwares/chatexample/LoginFragment.kt
+++ app/src/main/java/com/bsoftwares/chatexample/LoginFragment.kt
@@ -7,22 +7,37 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_login.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class LoginFragment : Fragment() {
 
+    private lateinit var auth : FirebaseAuth
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
+        auth = Firebase.auth
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        txt_register.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
     }
 }
--- app/src/main/java/com/bsoftwares/chatexample/RegisterFragment.kt
+++ app/src/main/java/com/bsoftwares/chatexample/RegisterFragment.kt
@@ -1,12 +1,29 @@
 package com.bsoftwares.chatexample
 
+import android.app.Activity
+import android.content.Intent
+import android.content.res.Resources
+import android.graphics.ImageDecoder
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.Toast
+import androidx.core.content.res.ResourcesCompat
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.fragment_register.*
+import java.util.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -14,15 +31,102 @@ import androidx.navigation.fragment.findNavController
 class RegisterFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        txt_login.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+        btn_register.setOnClickListener {
+            regUser(et_email_register.text.toString(), et_password_register.text.toString(),et_user_register.text.toString())
+        }
+        btn_selectPhoto.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, 0)
+        }
+        et_email_register.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
 
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                til_email_register.error = null
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+        })
+        et_password_register.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, total: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, total: Int, p2: Int, p3: Int) {
+                if (total >= 5)
+                    til_password.error = null
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        })
     }
+
+    var selectPhotoURI : Uri? = null
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (Activity.RESULT_OK == resultCode && data != null)
+            when (requestCode) {
+                0 -> {
+                    selectPhotoURI = data.data!!
+                    val bitmap = MediaStore.Images.Media.getBitmap(requireActivity().contentResolver,selectPhotoURI)
+                    civ_photo_selected.setImageBitmap(bitmap)
+                    btn_selectPhoto.alpha = 0f
+                }
+            }
+    }
+
+    private fun regUser(email: String, password: String,user:String) {
+        if (email.isEmpty() || password.isEmpty() || et_user_register.text.toString().isEmpty())
+            return
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+            email,
+            password
+        ).addOnCompleteListener {
+            if (!it.isSuccessful) {
+                when (val errorMessage = it.exception!!.message) {
+                    getString(R.string.error_email) -> til_email_register.error = errorMessage
+                    getString(R.string.error_characters) -> til_password.error = errorMessage
+                }
+                return@addOnCompleteListener
+            }
+            uploadImageToFireStorage(user)
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    private fun uploadImageToFireStorage(user:String) {
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("/images/$filename")
+        ref.putFile(selectPhotoURI!!).addOnSuccessListener {
+            saveUserToFirebaseDatabase(it.toString(),user)
+        }
+    }
+
+    private fun saveUserToFirebaseDatabase(profileImageUrl: String,user:String) {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+        ref.setValue(User(uid,user,profileImageUrl)).addOnSuccessListener {
+            Toast.makeText(requireContext(), "Criado", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    class User(val uid: String,val username: String,val profileImageUrl:String)
+
 }
